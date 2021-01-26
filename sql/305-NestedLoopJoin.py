d178071bd446e2dfdb9fb851a32cb3117479b647
wolfandthegang <wolfandthegang@users.noreply.github.com>
2021-01-26 10:48:29 -0500

    305 in Python

---
+++ sql/305-NestedLoopJoin.py
@@ -0,0 +1,35 @@
+'''
+Created on 3/23/2015
+Illustrates nested Loop Join in SQL
+cs3200 Database design
+https://northeastern-datalab.github.io/cs3200/
+__author__ = 'gatt'
+'''
+
+print "--- 1st nested loop ---"
+for i in xrange(2):
+    for j in xrange(3):
+        for k in xrange(2):
+            print "i=%d, j=%d, k=%d: " % (i, j, k),
+            if i == j or i == k:
+                print "TRUE",
+            print
+
+print "\n--- 2nd nested loop ---"
+for i in xrange(2):
+    for j in xrange(3):
+        for k in xrange(1):
+            print "i=%d, j=%d, k=%d: " % (i, j, k),
+            if i == j or i == k:
+                print "TRUE",
+            print
+
+print "\n--- 3rd nested loop ---"
+for i in xrange(2):
+    for j in xrange(3):
+        for k in xrange(0):
+            print "i=%d, j=%d, k=%d: " % (i, j, k),
+            if i == j or i == k:
+                print "TRUE",
+            print
+
